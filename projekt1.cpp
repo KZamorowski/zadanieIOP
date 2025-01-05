13cac7faa8d5b30e0c1b3da2a65233384f679cda
Kamil Mikolaj Zamorowski <kzamorowski@ultra60.mat.umk.pl>
2025-01-05 13:08:38 +0100

    Funckja uzycia

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Funkcja obliczająca silnię liczby
 int silnia(int n) {
     int wynik = 1;
     for (int i = 1; i <= n; ++i) {
@@ -10,10 +11,23 @@ int silnia(int n) {
     return wynik;
 }
 
-int main(){
-	int n = 5;
-	int wynik = silnia(n);
-	cout << wynik << endl;
-	return 0;
+int main() {
+    int wyjscie;
+    do {
+        cout << endl;
+        cout << "MENU" << endl;
+        cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "0. Wyjscie" << endl;
+        cout << "1. Oblicz silnię liczby" << endl;
+        cin >> wyjscie;
 
+        if (wyjscie == 1) {
+            int liczba;
+            cout << "Podaj liczbe: ";
+            cin >> liczba;
+            cout << "Silnia liczby " << liczba << " wynosi: " << silnia(liczba) << endl;
+        }
+    } while (wyjscie != 0);
+    return 0;
 }
+
